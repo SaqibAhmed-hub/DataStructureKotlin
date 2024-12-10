7e3f616ab147b2a86be31ba9a2d7efb8e926c638
Saqib Ahmed <saqibahmed@colanonline.com>
2024-12-10 11:46:00 +0530

    added some map functions and exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,16 @@
+import maps.BasicMaps
 
 fun main() {
     println("Welcome to Data Structure and Learning")
+    /**
+     *
+     * How to read the value in Kotlin
+     *
+     * To read the value
+     * val str = readln().trimEnd()
+     *
+     * To read an array
+     * val arr = readln().trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+     */
+
 }
